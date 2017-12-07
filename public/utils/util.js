b5023c7051062edfa4092d36335d1781fcd38a3f
zangse <zangse@live.com>
2017-12-07 16:48:08 +0800

    websocket通信

--- public/utils/util.js
+++ public/utils/util.js
@@ -108,19 +108,5 @@ util.jsonp = function(url, onsuccess, onerror) {
 }
 
 util.webSocket = function(url) {
-    var ws = new WebSocket(url);
 
-    ws.onopen = function(evt) {
-        console.log("Connection open ...");
-        ws.send("Hello WebSockets!");
-    };
-
-    ws.onmessage = function(evt) {
-        console.log("Received Message: " + evt.data);
-        ws.close();
-    };
-
-    ws.onclose = function(evt) {
-        console.log("Connection closed.");
-    };
 }
